4d557240ea5bfe4a9453b10dc7121f4f2b1683f2
Simon Sinclair <simonsinclair@users.noreply.github.com>
2017-03-03 17:02:06 +0000

    Friday chat, code improvs. and comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,54 @@
 // Logic for Tom's prototype.
 // ==========================
-$(document).ready(function(){
 
-	$( ".icon" ).click(function() {
-  $( this ).toggleClass('selected-icon');
-	$('.dl-bar').css('opacity', '1');
-	$('#selection-counter').html(function(i, val) { return val*1+1 });
-});
+$(document).ready(function() {
+	
+	var numSelectedIcons = 0;
+
+	$('.icon').click(function() {
+
+		var iconIsSelected = $(this).hasClass('selected-icon');
+
+		// If Icon has already been clicked, then:
+		if (iconIsSelected) {
+
+			// 1. Decrease the counter variable.
+			numSelectedIcons = numSelectedIcons - 1;
+
+			// 2. Remove the selected class.
+			$(this).removeClass('selected-icon');
+
+			// 3. Update the selection counter to our counter variable value.
+			$('#selection-counter').html(numSelectedIcons);
+
+			// 4. If our counter is less-than or equal to 0, ALSO
+			//     hide the download bar.
+			if (numSelectedIcons <= 0) {
+				$('.dl-bar').css('opacity', '0');
+			}
+
+		// If Icon has NOT been clicked, then:
+		} else {
+
+			// 1. Increase our counter variable.
+			numSelectedIcons = numSelectedIcons + 1;
+
+			// 2. Add selected class.
+			$(this).addClass('selected-icon');
+
+			// 3. Show the download bar.
+			$('.dl-bar').css('opacity', '1');
+
+			// 4. Update the selection counter with our counter variable value.
+			$('#selection-counter').html(numSelectedIcons);
+
+
+			// FOR DOWNLOAD
+			// - Get the src of the icon
+			// - and set th href of the DL link to it
+
+		}	
+
+	});
 
 });
