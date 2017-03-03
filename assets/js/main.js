96d41e3ce655eaaa5c5263078f94046cb980c45c
Tom Box <tom.box@bbc.co.uk>
2017-03-03 14:20:05 +0000

    used class instead of id & merged the two
    used class instead of id & merged the two

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,10 @@
 // ==========================
 $(document).ready(function(){
 
-	$( "#icon" ).click(function() {
-  $( this ).addClass('selected-icon');
+	$( ".icon" ).click(function() {
+  $( this ).toggleClass('selected-icon');
 	$('.dl-bar').css('opacity', '1');
-});
-
-$('#icon').click(function() {
-    $('#selection-counter').html(function(i, val) { return val*1+1 });
+	$('#selection-counter').html(function(i, val) { return val*1+1 });
 });
 
 });
